9ba6bbd91cb30fbf9fdc1068efe159d054b6914e
Levyslater <muthui@yandex.com>
2023-11-27 23:41:12 +0300

    linked_list

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+
+/**
+ * check_cycle - function checks if singly list is cyclic
+ * @list: head pointer
+ *
+ * Return: 1 if cyclic. 0 otherwise
+ */
+
+int check_cycle(listint_t *list)
+{
+	listint_t *ptr;
+
+	ptr = list->next;
+
+	while (ptr->next != NULL)
+	{
+		if (ptr->next == list)
+			return (1);
+		ptr = ptr->next;
+	}
+	return (0);
+}
