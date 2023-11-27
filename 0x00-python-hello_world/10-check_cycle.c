8ac0d1cf280a63d6c6e449954c39cd4a59f1339f
Levyslater <muthui@yandex.com>
2023-11-28 01:07:44 +0300

    Revert "cyclic2"
    This reverts commit 21b31276e9dfb0a38a5d1a353cae3aed9466617a.

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -9,16 +9,13 @@
 
 int check_cycle(listint_t *list)
 {
-	if (list == NULL)
-		return (1);
-
 	listint_t *ptr;
 
-	ptr = list->next;
+	ptr = list;
 
 	while (ptr != NULL)
 	{
-		if (ptr == list)
+		if (ptr->next == list)
 			return (1);
 		ptr = ptr->next;
 	}
