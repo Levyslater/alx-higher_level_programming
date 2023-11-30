694fd05a9d5ac7a470f5d598f945c31a7b5b39eb
Levyslater <muthui@yandex.com>
2023-11-30 23:55:57 +0300

    add2

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
-def add(a, b):
-
-    return a + b
-
-
 if __name__ == "__main__":
-    print("{} + {} = {}".format(1, 2, add(1, 2)))
+    """My print function. """
+    from add_0 import add
+
+    a = 1
+    b = 2
+    print("{} + {} = {}".format(a, b, add(a, b)))
