9e4f705a2c52a097702aac2d4bbfa1ee5f273029
Levyslater <muthui@yandex.com>
2023-12-04 00:40:06 +0300

    syntax2

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -2,6 +2,7 @@
 def new_in_list(my_list, idx, element):
     if idx < 0 or idx > len(my_list) - 1:
         return my_list
-    new_list = my_list.deepcopy(my_list)
+    new_list = [item if type(item) is not list else item[:]
+                for item in my_list]
     new_list[idx] = element
     return new_list
