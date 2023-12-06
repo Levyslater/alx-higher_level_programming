6b12c2b2a9f3b12cf573e5f55f491b8c21ff8d67
Levyslater <muthui@yandex.com>
2023-12-06 17:16:41 +0300

    lists_palindrome

---
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * is_palindrome - function checks if a list is palindrome
+ * @head: pointer to head
+ * Return: 1 if palindrome, 0 otherwise
+ */
+
+int is_palindrome(listint_t **head)
+{
+	listint_t *current = NULL;
+	unsigned int m;
+	int start = 0;
+	int arr[20];
+
+	current = *head;
+	m = 0;
+
+	while (current != NULL)
+	{
+		arr[m] = current->n;
+		current = current->next;
+		m++;
+	}
+	m -= m;
+	while (start < m)
+	{
+		if (arr[start] != arr[m])
+			return (0);
+		start++;
+		m--;
+	}
+	return (1);
+}
--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def print_reversed_list_integer(my_list=[]):
-    if my_list is None or len(my_list) < 1:
+    if my_list i6s None or len(my_list) < 1:
         return
     my_list.reverse()
     for i in my_list:
