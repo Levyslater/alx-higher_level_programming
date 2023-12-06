8eedd97393aaba5048fa2ea8a08bacd1061c2eeb
Levyslater <muthui@yandex.com>
2023-12-06 08:29:59 +0300

    chars_num

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
     for row in matrix:
-        for i in row:
-            print('{:d} '.format(i), end='')
-        print()
+        i = ' '.join(map(str, row))
+        print("{}".format(i))
