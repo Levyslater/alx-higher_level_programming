024db76ece76ef5b8fac1e31d113c63f5950a9ec
Levyslater <muthui@yandex.com>
2024-01-12 18:49:24 +0300

    attribute1

--- 0x0A-python-inheritance/101-add_attribute.py
+++ 0x0A-python-inheritance/101-add_attribute.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """Module adds new attribute if possible"""
 
+
 def add_attribute(attrib, name, value):
     """Add a new attribute to an object if possible"""
     if hasattr(attrib, '__dict__') or (hasattr(attrib, '__slots__') and name in attrib.__slots__):
