dae9c92bbf7636ce29f31c2c6fb7a8d489eee9da
Levyslater <lawre@Xshader>
2024-05-24 19:56:00 +0300

    more_queries7

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,3 +1,5 @@
-/*Script show permissions for user_0d_1 and user_0d_2 */
+/* Script show permissions for user_0d_1
+ and user_0d_2 
+ */
 SHOW GRANTS FOR 'user_0d_1'@'localhost';
 SHOW GRANTS FOR 'user_0d_2'@'localhost';
