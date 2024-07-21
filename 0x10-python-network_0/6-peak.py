db5fe5d1f0a52b2a027e339d1039040ecd306e9d
Levyslater <lawre@Xshader>
2024-07-21 13:08:46 +0300

    python network_0 rev1

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
-
-#Write a function that finds a peak in a list of unsorted integers
-
+"""
+Write a function that finds a peak in a list of unsorted integers
+"""
 def find_peak(list_of_integers):
     if not list_of_integers:
         return None
