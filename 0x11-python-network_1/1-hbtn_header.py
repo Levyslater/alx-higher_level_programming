447c610e9d6e1afb8095cf6f5240453e1bfe8e83
Levyslater <lawre@Xshader>
2024-07-21 17:33:46 +0300

    headers2

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -1,14 +1,37 @@
 #!/usr/bin/python3
 
 """
-a Python script that fetches the X-Request-Id header from a given URL
+A Python script that fetches the X-Request-Id header from a given URL
 """
+
 import urllib.request
 import sys
-url = sys.argv[1]
-response = urllib.request.Request(url)
-try:
-    with urllib.request.urlopen(response) as f:
-        print(f.headers['X-Request-Id'])
-except KeyError:
-    print("Error: X-Request-Id not found")
+
+
+def main():
+    """
+    main module
+    """
+    if len(sys.argv) != 2:
+        print("Usage: {} <URL>".format(sys.argv[0]))
+        sys.exit(1)
+
+    url = sys.argv[1]
+    request = urllib.request.Request(url)
+
+    try:
+        with urllib.request.urlopen(request) as response:
+            headers = response.getheaders()
+            x_request_id = dict(headers).get('X-Request-Id')
+            if x_request_id:
+                print(x_request_id)
+            else:
+                print("Error: X-Request-Id not found")
+    except urllib.error.URLError as e:
+        print("Error: Unable to fetch the URL. Reason:", e.reason)
+    except Exception as e:
+        print("Error:", str(e))
+
+
+if __name__ == "__main__":
+    main()
