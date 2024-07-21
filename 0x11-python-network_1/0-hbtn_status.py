24a5251fba52ad22675d14764603f9b6917fe589
Levyslater <lawre@Xshader>
2024-07-21 17:38:45 +0300

    pycodestyle

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,13 +1,26 @@
 #!/usr/bin/python3
 
 """
-a Python script that fetches https://alx-intranet.hbtn.io/status
+A Python script that fetches https://alx-intranet.hbtn.io/status
 """
 import urllib.request
-with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as response:
-   html = response.read()
-print('Body response:')
-print(f'    - type: {type(html)}')
-print(f'    - content: {html}')
-utf8_content = html.decode('utf8')
-print(f'    - utf8 content: {utf8_content}')
+
+
+def main():
+    """
+    main module
+    """
+    url = 'https://alx-intranet.hbtn.io/status'
+
+    with urllib.request.urlopen(url) as response:
+        html = response.read()
+
+    print("Body response:")
+    print(f"    - type: {type(html)}")
+    print(f"    - content: {html}")
+    utf8_content = html.decode('utf-8')
+    print(f"    - utf8 content: {utf8_content}")
+
+
+if __name__ == "__main__":
+    main()
